829512e64dbeff068a31092094a31aed49c1aebf
taehyeon130 <taehyeongim59@gmail.com>
2021-04-09 11:48:03 +0900

    n

--- day0408/server.js
+++ day0408/server.js
@@ -13,9 +13,11 @@ var ejs = require("ejs");
 var app = express();
 
 // 필요한 각종 미들웨어 적용 static자원의 루트를 입력하는 것
+// console.log("호호호ㅗㅎ",__dirname);
 app.use(express.static(__dirname+"/static"));
 
 // 업로드 모듈을 이용한 업로드 처리 destination는 저장할 곳 filename 저장할 이름
+// 노드 js뿐만아니라 asp,php,jsp등은 일단 업로드 컴포넌트를 사용할 경우 모든 post는 이 업로드 컴포넌트를 통해 처리된다...
 var upload = multer({
     storage:multer.diskStorage({
         destination:function(request,file,cb){
@@ -93,48 +95,120 @@ app.get("/gallery/detail",function(request,response){
     var con = mysql.createConnection(conStr);
     var gallery_id= request.query.gallery_id;
 
-    var  sql = "select *from gallery where gallery_id="+gallery_id;
+    var  sql ="select *from gallery where gallery_id="+'gallery_id';
 
     // 쿼리문 수행
     con.query(sql,function(err,result,fields){
         if(err){
             console.log(err);
         }else{
-            fs.readFile("./gallery/detail.ejs","utf8",function(error,data){
-                if(error){
-                    console.log(error);
+            con.query("update gallery set hit=hit+1 where gallery_id =?",gallery_id,function(err,fields){
+                if(err){
+                    console.log(err);
                 }else{
-                    var d = ejs.render(data,{
-                        gallery:result[0]//result가 한건의 데이터만 담고있다고 하더라도 배열이므로
+                    fs.readFile("./gallery/detail.ejs","utf8",function(error,data){
+                        if(error){
+                            console.log(error);
+                        }else{
+                            var d = ejs.render(data,{
+                                gallery:result[0]//result가 한건의 데이터만 담고있다고 하더라도 배열이므로
+                            });
+                            response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+                            response.end(d);
+                        }
+                        con.end();
                     });
-                    response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
-                    response.end(d);
                 }
-                con.end();
             });
         }
     });
 });
 
 // 삭제요청 처리(DB삭제 + 이미지 삭제)
-app.get("/gallery/del",function(request,response){
-    console.log(request.body);
-    var gallery_id = request.query.gallery_id;
-    var filename = request.query.filename
-    fs.unlink(__dirname+"/static/upload"+filename,function(err){
-        console.log("삭제완료");
-    });
-    // var gallery_id = request.body.gallery_id; //post 방식의 파라미터 추출
+// 파일을 업로드 하건 안하건 일단 multer를 사용하게 되면 모든 post방식에 관여하게 되어있다
+// node.js....
+app.post("/gallery/del",upload.single("pic"),function(request,response){
+    var gallery_id = request.body.gallery_id;
+    var filename = request.body.filename;
 
-    // var sql="delete from gallery where gallery_id="+gallery_id;
-    response.end(sql);
+    console.log("gallery_id",gallery_id);
+    fs.unlink(__dirname+"/static/upload/"+filename,function(err){
+       if(err){
+           console.log(err);
+       }else{
+           console.log("삭제완료");
+            //db도 지우자!!
+            var sql ="delete from gallery where gallery_id="+gallery_id;
+            var con = mysql.createConnection(conStr);
+            con.query(sql,function(error,fields){
+                if(error){
+                    console.log(error);
+                }else{
+                    // 목록 요청
+                    response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+                    response.end(mymodule.getMsgUrl("삭제완료","/gallery/list"));
+                }
+                con.end();
+            });
+       }
+    });
+});
+
+// 글 수정하기(서버가 일단 업로드 컴포넌트를 사용하게 되면 post는 무조건 업로드 컴포넌트를 이용해야한다)
+app.post("/gallery/edit",upload.single("pic"),function(request,response){
+    var title=request.body.title;
+    var writer=request.body.writer;
+    var content=request.body.content;
+    var gallery_id = request.body.gallery_id;
+    var filename = request.body.filename
+
+    //업로드시 request객체의 json속성 중 file이라는 속성이 판단 대상
+    // 클라이언트가 업로드를 원하는지 않하는지를 구분?
+    // console.log(request.file); //수정버튼만 누르면 file속성 없고 사진을 바꾸고 수정버튼을 누르면 flle속성이 나타남
+    // 사진도 변경
+    
+    // 사진 유지
+    
+    if(request.file != undefined){ //업로드를 원함(사진 교체)
+        // response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+        console.log("사진 교체");
+        // 사진 지우기 + db수정
+        fs.unlink(__dirname+"/static/upload/"+filename,function(err){
+            if(err){
+                console.log(err);
+            }else{
+                filename=request.file.filename; //새롭게 업로드된 파일명으로 교체
+                var sql ="update gallery set title=?, writer=?, content=?, filename=? where gallery_id = ?";
+
+                var con =mysql.createConnection(conStr);
+                con.query(sql,[title,writer,content,filename,gallery_id],function(err,fields){
+                    if(err){
+                        console.log(err);
+                    }else{
+                        response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+                        response.end(mymodule.getMsgUrl("수정완료","/gallery/detail?gallery_id=",gallery_id));
+                    }
+                    con.end();
+                });
+            }
+        }); 
+    }else{ //사진 유지
+        console.log("사진 유지");
+        var sql="update gallery set title=?,writer=?,content=? where gallery_id=?";
+        var con = mysql.createConnection(conStr);
+        con.query(sql,[title,writer,content,gallery_id],function(error,fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+                response.end(mymodule.getMsgUrl("수정완료","/gallery/detail?gallery_id=",gallery_id));
+            }
+            con.end();
+        });
+    }
 });
 
 var server = http.createServer(app);//기본 모듈에 express모듈 연결
  server.listen(9999,function(){
     console.log("Gallery Server is running at 9999Port...");
  });
-
-
-
-
